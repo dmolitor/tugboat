f054dbf6d8c3b8935697051550a6b2348e3a6f5c
Daniel Molitor <molitdj97@gmail.com>
2024-12-14 23:01:54 -0500

    Improve tests :broom:

--- tests/testthat/test-build.R
+++ tests/testthat/test-build.R
@@ -1,9 +1,9 @@
-test_that("Building the Docker image works as expected", {
-  testthat::skip_on_ci()
+test_that("Expected installed packages are in the Docker image", {
   testthat::skip_on_cran()
-  if (!interactive()) {
-    testthat::skip()
+  if (!interactive() && Sys.info()[["sysname"]] != "Linux") {
+    skip("Tests only run interactively or on Ubuntu (Linux)")
   }
+  
   image_name <- build(
     dockerfile = here::here("examples/simple/Dockerfile"),
     image_name = "tugboat_simple",
@@ -28,4 +28,45 @@ test_that("Building the Docker image works as expected", {
     c("jsonlite", "renv", "rprojroot", "stringi", "withr"),
     image_packages
   )
+})
+
+test_that("Dockerfile in a different directory than build context works correctly", {
+  testthat::skip_on_cran()
+  if (!interactive() && Sys.info()[["sysname"]] != "Linux") {
+    skip("Tests only run interactively or on Ubuntu (Linux)")
+  }
+
+  if (file.exists(here::here("examples/diff_dockerfile_and_context/docker/Dockerfile"))) {
+    file.remove(here::here("examples/diff_dockerfile_and_context/docker/Dockerfile"))
+  }
+  exclude <- c("./mtcars.csv")
+  dock <- create(
+    project = here::here("examples/diff_dockerfile_and_context/"),
+    as = here::here("examples/diff_dockerfile_and_context/docker/Dockerfile"),
+    FROM = "r-base",
+    exclude = exclude
+  )
+
+  # Build the docker image
+  image_name <- build(
+    dockerfile = here::here("examples/diff_dockerfile_and_context/docker/Dockerfile"),
+    image_name = "tugboat_diff_df_context",
+    platforms = "linux/arm64",
+    build_context = here::here("examples/diff_dockerfile_and_context/")
+  )
+
+  # Check copied files
+  files <- system(
+    paste0(
+      "docker run --rm ",
+      image_name,
+      " ls -la /diff_dockerfile_and_context"
+    ),
+    intern = TRUE
+  ) |> strsplit(split = " ") |> 
+    (\(x) lapply(x, \(y) y[[length(y)]]))() |>
+    unlist()
+
+  # Ensure that all expected packages are installed
+  testthat::expect_false(exclude %in% files)
 })

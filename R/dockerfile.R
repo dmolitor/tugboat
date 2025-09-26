a6f0a32a630cea499175628f761ece8fc8de5d73
Daniel Molitor <molitdj97@gmail.com>
2025-09-26 17:55:45 -0400

    Make tugboat set up `pak` to use binaries better 📦

--- R/dockerfile.R
+++ R/dockerfile.R
@@ -9,12 +9,12 @@ dockerfile <- function(lockfile, FROM = NULL) {
   if (is.null(FROM)) FROM <- paste0("r-base:", Rversion)
   dock <- c(
     paste("FROM", FROM),
-    "RUN mkdir -p /usr/local/lib/R/etc/ /usr/lib/R/etc/",
-    "RUN echo \"options(renv.config.pak.enabled = TRUE, repos = c(CRAN = 'https://cran.rstudio.com/'), download.file.method = 'libcurl', Ncpus = 4)\" | tee /usr/local/lib/R/etc/Rprofile.site | tee /usr/lib/R/etc/Rprofile.site",
-    "RUN R -e \"install.packages('pak', repos = sprintf('https://r-lib.github.io/p/pak/stable/%s/%s/%s', .Platform[['pkgType']], R.Version()[['os']], R.Version()[['arch']]))\"",
-    paste0("RUN R -e \"pak::pkg_install('", renv_version, "')\""),
     paste("COPY", basename(lockfile), "renv.lock"),
-    paste("RUN R -e \"renv::restore()\"")
+    "RUN mkdir -p /usr/local/lib/R/etc/ /usr/lib/R/etc/ && \\",
+    "    for f in /usr/local/lib/R/etc/Rprofile.site /usr/lib/R/etc/Rprofile.site; do \\\n      echo \"options(renv.config.pak.enabled = TRUE)\" >> \"$f\"; \\\n    done",
+    "RUN R -e \"install.packages('pak', repos = sprintf('https://r-lib.github.io/p/pak/stable/%s/%s/%s', .Platform[['pkgType']], R.Version()[['os']], R.Version()[['arch']]))\"",
+    paste0("RUN R -e \"dist <- pak::system_r_platform_data()[['distribution']]; rel <- pak::system_r_platform_data()[['release']]; binary_url <- subset(pak::ppm_platforms(), distribution == dist & release == rel)[['binary_url']][1]; cran_binary_url <- if (!is.na(binary_url)) { sprintf('%s/__linux__/%s/latest', pak::ppm_repo_url(), binary_url)  } else { NA }; if (!is.na(cran_binary_url)) { pak::repo_add(CRAN = cran_binary_url) }; ", paste0("pak::pkg_install('", renv_version, "')"), "; if (!is.na(cran_binary_url)) { renv::lockfile_modify(repos = c('CRAN' = cran_binary_url)) |> renv::lockfile_write('./renv.lock') }\""),
+    "RUN R -e \"renv::restore()\""
   )
   return(dock)
 }
